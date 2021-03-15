20783abab04b17ad33eab56ed53431ac591b68cd
BACHTIAR8987854 <amminudinbahtiar101@gmail.com>
2021-03-16 01:45:17 +0700

    mengatasi error login

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 // root
-Route::get('/',function(){
+Route::get('/dashboard',function(){
     return view('index');
 });
 
-Route:: get   ('crud','App\Http\Controllers\CrudController@index')         ->name('crud');
-Route:: get   ('crud/tambah','App\Http\Controllers\CrudController@tambah') ->name('crud.tambah');
-Route:: post  ('crud','App\Http\Controllers\CrudController@simpan')        ->name('crud.simpan');  
-Route:: delete('crud/{id}','App\Http\Controllers\CrudController@delete')  ->name('crud.delete');
+
+Route:: get   ('/','App\Http\Controllers\otentikasi\OtentikasiController@login')->name('login');
+
+Route:: get   ('crud','App\Http\Controllers\CrudController@index')->name('crud');
+Route:: get   ('crud/tambah','App\Http\Controllers\CrudController@tambah')->name('crud.tambah');
+Route:: post  ('crud','App\Http\Controllers\CrudController@simpan')->name('crud.simpan');  
+Route:: delete('crud/{id}','App\Http\Controllers\CrudController@delete')->name('crud.delete');
 Route:: get   ('crud/{id}/edit','App\Http\Controllers\CrudController@edit')->name('crud.edit');
-Route:: patch ('crud/{id}','App\Http\Controllers\CrudController@update')   ->name('crud.update');
+Route:: patch ('crud/{id}','App\Http\Controllers\CrudController@update')->name('crud.update');
 
 
     
